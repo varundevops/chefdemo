b18f463f5eaa9fa7dd57b6a4e13c79c44731a044
Varun@example.com <root@workstation.(none)>
2016-11-03 13:23:56 +0530

    test

---
+++ chef-repo/cookbooks/apache/attributes/default.rb
@@ -0,0 +1,11 @@
+default["apache"]["sites"]["varun"] = { "site_title" => "Varun website coming soon", "port" => 80, "domain" => "varun.com" }
+default["apache"]["sites"]["varun2"] = { "site_title" => "Varun2 website coming soon", "port" => 80, "domain" => "varun2.com" }
+default["apache"]["sites"]["varun3"] = { "site_title" => "Varun3 website coming soon", "port" => 80, "domain" => "varun3.com" }
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
+
